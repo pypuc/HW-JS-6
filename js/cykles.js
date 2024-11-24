20e1864bf263c55c06e0c911e8788d9d7bee6c4b
Іван Довгаль <145868278+pypuc@users.noreply.github.com>
2024-11-24 14:45:52 +0200

    fix

--- js/cykles.js
+++ js/cykles.js
@@ -1,1 +1,63 @@
-const recountNumbers = ""
+// 1 завдання
+// let i = 1
+//  while (i <= 10){
+//      alert(i)
+//      i++
+//  }
+
+// 2 завдання
+//  for (let f = 2; f <= 20; f += 1){
+//      if (f % 2 === 0){
+//          alert(f)
+//      }
+//  }
+
+// 3 завдання
+
+// const  number = 7;
+// for (let i = 0; i <= 10; i += 1) {
+//   alert(`${number} * ${i} = ${number * i}`);
+// }
+
+// 4 завдання
+// const numbers = [1, 2, 3, 4, 5];
+// let total = 0;
+// while (total < numbers.length) {
+//     alert(numbers[total]); 
+//     total++; 
+// }
+
+// 5 завдання
+// let number = 1;
+// while (number <= 20) {
+//     if (number % 3 === 0) {
+//         number++;
+//         continue;
+//     }
+//     alert(number);
+//     number++;
+// }
+
+// 6 завдання
+// let n = 12;
+// let numberr = 1;
+// while (true){
+//     if (numberr >= n){
+//     break
+// }
+//  alert(numberr)
+//  numberr++;
+// }
+
+// 7
+// let tree = 1; 
+
+// while (tree <= 20) {
+//     if (tree % 3 === 0) {
+//         tree++; 
+//         continue;
+//     }
+//     alert(tree);
+//     number++; 
+// }
+
